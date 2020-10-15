6c994bc3bf60da4a8d89d8a71d1081e38bcfb488
Julien <julien.best@live.de>
2020-10-15 13:01:32 +0200

    Fixed circle paint method, moving is now working

--- src/irmb/flowsim/view/graphics/PaintableCircle.java
+++ src/irmb/flowsim/view/graphics/PaintableCircle.java
@@ -16,17 +16,33 @@ public class PaintableCircle extends PaintableShape {
         this.circle = circle;
     }
 
-    @Override
+    /*@Override
     public void paint(Painter painter, CoordinateTransformer transformer) {
         painter.setColor(Color.BLACK);
         this.transformer = transformer;
         Point center = transformer.transformToPointOnScreen(this.circle.getCenter());
         painter.paintCircle(center.getX(), center.getY(), transformer.scaleToScreenLength(this.circle.getRadius()));
+    }*/
+
+    public void paint(Painter painter, CoordinateTransformer transformer) {
+        Point center = circle.getCenter();
+        double radius = circle.getRadius();
+        double min_x = transformer.transformToPointOnScreen(new Point(center.getX() - radius, center.getY() - radius)).getX();
+        double max_x = transformer.transformToPointOnScreen(new Point(center.getX() + radius, center.getY() + radius)).getX();
+        double view_radius = 0.5*(max_x - min_x);
+        Point view = transformer.transformToPointOnScreen(center);
+        painter.setColor(Color.BLACK);
+        painter.paintCircle(view.getX()-view_radius, view.getY()-view_radius, 2*view_radius);
     }
 
     @Override
     public boolean isPointOnBoundary(Point point, double radius) {
-        return false;
+        double min_distance = this.circle.getRadius() - radius;
+        double max_distance = this.circle.getRadius() + radius;
+        Point center = this.circle.getCenter();
+        double distance = Math.sqrt((center.getX() - point.getX()) * (center.getX() - point.getX()) + (center.getY() - point.getY()) * (center.getY() - point.getY()));
+        System.out.println(min_distance + " <= " + distance + " <= " + max_distance);
+        return (distance >= min_distance && distance <= max_distance);
     }
 
     @Override
