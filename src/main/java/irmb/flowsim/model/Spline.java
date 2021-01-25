ca33a6e1e200cb4c3fdc1319a1c341a6a6640ea0
Julien <julien.best@live.de>
2021-01-25 20:33:04 +0100

    Adding starting point for Ex07

--- src/main/java/irmb/flowsim/model/Spline.java
+++ src/main/java/irmb/flowsim/model/Spline.java
@@ -2,35 +2,67 @@ package irmb.flowsim.model;
 
 import java.util.List;
 
-public class Spline implements MultiPointShape {
+import Jama.Matrix;
 
-    @Override
-    public void addPoint(Point point) {
-        //TODO
+public class Spline extends PolyLine {
+
+    private Matrix x;
+
+
+    public Point getPointOnSpline(double t) {
+        //stelle in bezug zur gesamtlaenge n
+        
+        /* in welchem spline sind wir gerade? */
+     
+        //wie ist der versatz auf den aktuellen spline bezogen?
+      
+        //abfangen falls er bei num = n+1 landet...
+        //allerletzter punkt gehört spline n...
+
+        return new Point(0, 0);
+    }
+
+
+    private void calculateCoefficients() {
+
+        if (getPointList().size() > 2) {
+            int numOfSplines = getPointList().size() - 1;
+            int numOfCoeff = numOfSplines * 8;
+
+            Matrix A = new Matrix(numOfCoeff, numOfCoeff);
+            Matrix b = new Matrix(numOfCoeff, 1); // rechte Seite
+
+            /* schleife fuer jeden spline */
+            
+
+                /* erste gleichung - anfangspunkte */
+                
+
+                /* zweite gleichung - endpunkte */
+            
+
+                /* dritte gleichung - stetigkeit 1. abl */
+
+
+                /* vierte gleichung - stetigkeit 2. abl */
+        }
     }
 
     @Override
-    public void setLastPoint(Point point) {
-        //TODO
+    public void addPoint(Point point) {
+        super.addPoint(point);
+        calculateCoefficients();
     }
 
     @Override
     public void removeLastPoint() {
-        //TODO
-    }
-
-    @Override
-    public List<Point> getPointList() {
-        return null;
+        super.removeLastPoint();
+        calculateCoefficients();
     }
 
     @Override
     public void moveBy(double dx, double dy) {
-        //TODO
-    }
-
-    @Override
-    public void accept(ShapeVisitor visitor) {
-        visitor.visit(this);
+        super.moveBy(dx, dy);
+        calculateCoefficients();
     }
 }
